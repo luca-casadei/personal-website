49d366a77e5aa0040d070de5eaf5e92489627f34
Luca Casadei <casadeiluca30@gmail.com>
2023-10-19 18:49:43 +0200

    Retitled website, removed unused images, created some basic components to be implemented in future commits

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,7 @@
+export default function NavBar(){
+    return(
+        <nav className="amber-500">
+            NavBar!
+        </nav>
+    )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import Root from './Root'
+import { createBrowserRouter, RouterProvider} from 'react-router-dom'
+
+const router = createBrowserRouter([{
+  path:"/",
+  element: <Root/>
+}])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router}/>
 )
