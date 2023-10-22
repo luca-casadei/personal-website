a5084150894ba006e446a414a017383c6a4af6c4
Luca Casadei <casadeiluca30@gmail.com>
2023-10-23 01:22:30 +0200

    Styled the publications sections with tailwind classes.

--- src/components/HomePage/Publications.tsx
+++ src/components/HomePage/Publications.tsx
@@ -1,6 +1,21 @@
-export default function Publications(){
-    return(
-        <>
-        </>
-    )
+export default function Publications() {
+  return (
+    <section id="publications" className="mt-20 text-white">
+      <div className="flex md:flex-row flex-col-reverse mx-5 md:mx-20">
+        <div className="flex flex-col items-center">
+          <h1 className="text-2xl text-center md:mt-0 mt-3">
+            Pubblicazioni e documenti
+          </h1>
+          <p className="mt-3 md:mr-10 md:text-justify text-center">
+            In questa sezione verranno caricati documenti da me realizzati
+            durante la mia esperienza scolastica o progettuale che ritengo possa
+            essere utile condividere pubblicamente, tutto il contenuto caricato
+            qui non potrà essere utilizzato per nessun motivo a scopo
+            commerciale e senza
+          </p>
+        </div>
+        <img src="./doc.svg" alt="docImg" className="max-h-24 max-w-24" />
+      </div>
+    </section>
+  );
 }
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,9 +1,11 @@
 import PortfolioDescription from "../components/HomePage/PortfolioDescription";
+import Publications from "../components/HomePage/Publications";
 
 export default function Home() {
   return (
     <section id="homePage" className="flex flex-col mt-20 items-center text-white">
       <PortfolioDescription/>
+      <Publications/>
     </section>
   );
 }
