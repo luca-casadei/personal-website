b621f0cdcfa03d8fa0d752de87a068e6b24b0285
Luca Casadei <casadeiluca30@gmail.com>
2023-10-26 01:03:58 +0200

    Removed unused imports and fake loading page.

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,7 +1,6 @@
 //React imports
 import { Outlet } from "react-router-dom";
 import { useState } from "react";
-import {useEffect} from "react"
 
 //Navbar and footer
 import NavBar from "./components/NavBar";
@@ -10,21 +9,10 @@ import Footer from "./components/Footer";
 //Loading
 import LoadingContext from "./utilities/LoadingContext";
 import LoadingPage from "./errors/LoadingPage";
-import { sleep } from "./utilities/Sleepers";
 
 export default function Root() {
   const [isLoading, setLoading] = useState(false);
   const [loadingText, setLoadingText] = useState("");
-
-  //To test the loading page uncomment the code below.
-  /*useEffect(()=>{
-    setLoadingText("Debug della pagina di loading...");
-    setLoading(true);
-    sleep(3000).then(()=>{
-      setLoading(false);
-    })
-  },[])*/
-  
 
   return (
     <LoadingContext.Provider
