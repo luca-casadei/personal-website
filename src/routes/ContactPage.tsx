5104fd6ec3fa858c6fc30944525beb7b74288708
Luca Casadei <casadeiluca30@gmail.com>
2023-11-06 14:21:36 +0100

    Changed contact page background

--- src/routes/ContactPage.tsx
+++ src/routes/ContactPage.tsx
@@ -2,7 +2,7 @@ import MailForm from "../components/ContactPage/MailForm";
 
 export default function Contatti() {
   return (
-    <section id="contactPage" className="flex flex-col justify-center items-center bg-oxford-blue h-full">
+    <section id="contactPage" className="flex flex-col justify-center items-center h-full">
       <MailForm />
     </section>
   );
